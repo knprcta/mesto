a31bf87ed05dca3321a78ec2d6f9e3172fa29293
Kirill Fedotov <knprcta@yandex.ru>
2021-02-14 16:18:38 +0300

    fix: исправить ошибки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,54 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-const elementsContainer = document.querySelector('.elements');
-const templateEl = document.querySelector('.template');
+const elementsContainer = document.querySelector(".elements");
+const templateEl = document.querySelector(".template");
 
 function getItem(item) {
   const newItem = templateEl.content.cloneNode(true);
-  const itemImage = newItem.querySelector('.element__image');
-  const imgCaption = newItem.querySelector('.element__caption');
-  const likeButton = newItem.querySelector('.element__like-button');
-  const deleteButton = newItem.querySelector('.element__delete-button');
+  const itemImage = newItem.querySelector(".element__image");
+  const imgCaption = newItem.querySelector(".element__caption");
+  const likeButton = newItem.querySelector(".element__like-button");
+  const deleteButton = newItem.querySelector(".element__delete-button");
   itemImage.src = item.link;
   itemImage.alt = item.name;
   imgCaption.textContent = item.name;
-  likeButton.addEventListener('click', function () {
-    likeButton.classList.toggle('element__like-button_active');
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("element__like-button_active");
   });
-  deleteButton.addEventListener('click', mestoDelete);
-  itemImage.addEventListener('click', () => {
-    picOpen(item)
+  deleteButton.addEventListener("click", deletePlace);
+  itemImage.addEventListener("click", () => {
+    openPic(item);
   });
 
   return newItem;
@@ -55,84 +61,82 @@ function render() {
 
 render();
 
-let popupProfile = document.querySelector('.popup_profile');
-let formProfile = popupProfile.querySelector('.popup__container');
-let editButton = document.querySelector('.profile__edit-button');
-let nameInput = formProfile.querySelector('.popup__input_name');
-let jobInput = formProfile.querySelector('.popup__input_job');
-let name = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__job');
-let closeButtonProfile = formProfile.querySelector('.popup__close-button');
+let popupProfile = document.querySelector(".popup_profile");
+let formProfile = popupProfile.querySelector(".popup__container");
+let editButton = document.querySelector(".profile__edit-button");
+let nameInput = formProfile.querySelector(".popup__input_name");
+let jobInput = formProfile.querySelector(".popup__input_job");
+let name = document.querySelector(".profile__name");
+let job = document.querySelector(".profile__job");
+let closeButtonProfile = formProfile.querySelector(".popup__close-button");
 
-
-function popupOpen(popup) {
-  popup.classList.add('popup_opened');
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
 }
 
-function popupClose(popup) {
-  popup.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
 }
 
-function profileEdit() {
-  popupOpen(popupProfile);
+function editProfile() {
+  openPopup(popupProfile);
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
-
 }
 
-function formSubmitHandler(evt) {
+function handleProfileSubmit(evt) {
   evt.preventDefault();
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
-  popupClose(popupProfile);
+  closePopup(popupProfile);
 }
 
-let addButton = document.querySelector('.profile__add-button');
-let popupMesto = document.querySelector('.popup_mesto');
-let formMesto = popupMesto.querySelector('.popup__container');
-let titleInput = formMesto.querySelector('.popup__input_title');
-let linkInput = formMesto.querySelector('.popup__input_link');
-let closeButtonMesto = formMesto.querySelector('.popup__close-button');
+let addButton = document.querySelector(".profile__add-button");
+let popupPlace = document.querySelector(".popup_mesto");
+let formPlace = popupPlace.querySelector(".popup__container");
+let titleInput = formPlace.querySelector(".popup__input_title");
+let linkInput = formPlace.querySelector(".popup__input_link");
+let closeButtonPlace = formPlace.querySelector(".popup__close-button");
 
-function mestoAdd(evt) {
+function addPlace(evt) {
   evt.preventDefault();
-  const newMesto = getItem({ name: titleInput.value, link: linkInput.value });
-  elementsContainer.prepend(newMesto);
-  popupClose(popupMesto);
-  titleInput.value = '';
-  linkInput.value = '';
+  const newPlace = getItem({ name: titleInput.value, link: linkInput.value });
+  elementsContainer.prepend(newPlace);
+  closePopup(popupPlace);
+  titleInput.value = "";
+  linkInput.value = "";
 }
 
-function mestoDelete(evt) {
-  const targetEl = evt.target.closest('.element');
+function deletePlace(evt) {
+  const targetEl = evt.target.closest(".element");
   targetEl.remove();
 }
 
-let popupPic = document.querySelector('.popup_pic');
-let popupFig = popupPic.querySelector('.popup__figure');
-let popupImage = popupFig.querySelector('.popup__image');
-let popupCaption = popupFig.querySelector('.popup__caption');
-let closeButtonPic = popupFig.querySelector('.popup__close-button');
+let popupPic = document.querySelector(".popup_pic");
+let popupFig = popupPic.querySelector(".popup__figure");
+let popupImage = popupFig.querySelector(".popup__image");
+let popupCaption = popupFig.querySelector(".popup__caption");
+let closeButtonPic = popupFig.querySelector(".popup__close-button");
 
-function picOpen(item) {
-  popupOpen(popupPic);
+function openPic(item) {
+  openPopup(popupPic);
   popupImage.src = item.link;
   popupImage.alt = item.name;
   popupCaption.textContent = item.name;
 }
 
-editButton.addEventListener('click', profileEdit);
-addButton.addEventListener('click', () => {
-  popupOpen(popupMesto)
+editButton.addEventListener("click", editProfile);
+addButton.addEventListener("click", () => {
+  openPopup(popupPlace);
 });
-formProfile.addEventListener('submit', formSubmitHandler);
-formMesto.addEventListener('submit', mestoAdd);
-closeButtonProfile.addEventListener('click', () => {
-  popupClose(popupProfile)
+formProfile.addEventListener("submit", handleProfileSubmit);
+formPlace.addEventListener("submit", addPlace);
+closeButtonProfile.addEventListener("click", () => {
+  closePopup(popupProfile);
 });
-closeButtonMesto.addEventListener('click', () => {
-  popupClose(popupMesto)
+closeButtonPlace.addEventListener("click", () => {
+  closePopup(popupPlace);
 });
-closeButtonPic.addEventListener('click', () => {
-  popupClose(popupPic)
+closeButtonPic.addEventListener("click", () => {
+  closePopup(popupPic);
 });
