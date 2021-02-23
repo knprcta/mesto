f76f043f86f280d20de85b0610aaf7130c76df34
Kirill Fedotov <knprcta@yandex.ru>
2021-02-23 21:26:59 +0300

    feat: добавить состояние кнопки при валидации

--- scripts/validate.js
+++ scripts/validate.js
@@ -25,18 +25,32 @@ const checkInputValidity = (formElement, inputElement) => {
   };
 };
 
+const toggleButtonState = (inputList, buttonElement) => {
+  const hasNoValidInput = inputList.some(inputElement => !inputElement.validity.valid);
+
+  if (hasNoValidInput) {
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.removeAttribute('disabled');
+  };
+};
+
 const setEventListeners = (formElement) => {
   formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
   });
 
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__submit-button');
 
   inputList.forEach(inputElement => {
     inputElement.addEventListener('input', (evt) => {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
+  
+  toggleButtonState(inputList, buttonElement);
 };
 
 
